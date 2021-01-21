e3549b6dced9e176dd0e4445d598868c708991b1
JosuContrer <jdcontrerasalbuj@wpi.edu>
2021-01-21 00:13:55 -0500

    Added project slider and box shadows.

--- SourceCode/src/components/Hobbies.js
+++ SourceCode/src/components/Hobbies.js
@@ -1,32 +1,69 @@
 import React from 'react'
 
+import {Carousel} from "react-responsive-carousel";
+import ReactPlayer from "react-player";
 import {volunteeringImages, artImages} from '../data/HobbiesData.js';
 
 import './Hobbies.scss';
+import './carousel.css';
 
 export const Hobbies = () => {
+
+    const urls = [
+        "https://www.youtube.com/watch?v=_vmEgLo3z7Y&ab_channel=JosueContreras",
+        "https://www.youtube.com/watch?v=vGmk3REEqqA&fbclid=IwAR1k6oYCKJ-fMRITKb0wGbfZg_00wsLKZCk-MpWUcipKdwFFr4EZrG1Omu8&ab_channel=JonathanTai",
+        "https://www.youtube.com/watch?v=ev9uFGZMw5I&ab_channel=JosueContreras",
+    ];
+
+    const YoutubeSlide = ({ url, isSelected }: { url: string; isSelected?: boolean }) => (
+        <ReactPlayer width="100%" light={true} height="500px" controls={true} url={url} playing={isSelected} />
+    );
+
     return (
         <div className="HobbiesPage">
             <h1>Hobbies</h1>
             <div className="Container">
-                <div className="GridVolunteering">
-                    {
-                        volunteeringImages.map((item, index) => 
-                            <img className={item.title} src={item.image} key={index}/>
-                        )
-                    }
-                </div>
-                <div className="GridArt">
-                    {
-                        artImages.map((item, index) =>
-                            <img className={item.title} src={item.image} key={index}/>
-                        )
-                    }
-                </div>
-                <div className="GridPhotography">
-
-                </div>
+                {/*<div className="GridWrapper">*/}
+                {/*    <div className="TitleWrapper1">*/}
+                {/*        <h1>Volunteering</h1>*/}
+                {/*    </div>*/}
+                    <div className="GridVolunteering ShadowWrapper">
+                        {
+                            volunteeringImages.map((item, index) =>
+                                <img className={item.title} src={item.image} key={index}/>
+                            )
+                        }
+                    </div>
+                {/*</div>*/}
+                {/*<div className="GridWrapper">*/}
+                {/*    <div className="TitleWrapper1">*/}
+                {/*        <h1>Art</h1>*/}
+                {/*    </div>*/}
+                    <div className="GridArt ShadowWrapper">
+                        {
+                            artImages.map((item, index) =>
+                                <img className={item.title} src={item.image} key={index}/>
+                            )
+                        }
+                    </div>
+                {/*</div>*/}
+                {/*<div className="GridWrapper">*/}
+                {/*    <div className="TitleWrapper2">*/}
+                {/*        <h1>Cinematography</h1>*/}
+                {/*    </div>*/}
+                    <div className="GridPhotoCine ShadowWrapper">
+                        <Carousel>
+                            {
+                                urls.map((item, index) =>
+                                    <YoutubeSlide key={"youtube-"+{index}} url={item}/>
+                                )
+                            }
+                        </Carousel>
+                    </div>
+                {/*</div>*/}
             </div>
         </div>
+
+
     )
 }
--- SourceCode/src/components/ProjectsCard.js
+++ SourceCode/src/components/ProjectsCard.js
@@ -1,50 +1,81 @@
 import React from 'react'
 
+import {Carousel} from "react-responsive-carousel";
 import {computerScienceData} from '../data/CSProjectsData.js';
 import {roboticsData} from '../data/RBEProjectsData.js';
 
+import './carousel.css';
 import './ProjectsCard.scss';
 
 function ProjectsCard() {
+
+    const listItem = (length, index) => {
+        if(index !== length){
+           return ", ";
+        }
+    }
     return (
         <div className="ProjectsContainer">
             <h1 className="CardTitle ProjectsTextWrapper">Projects</h1>
             <h2 className="CardSubTitle">Computer Science</h2>
             <div className="ContentContainer">
-                {
-                    computerScienceData.map((item, index) =>
-                        <div className="ContentProfile" key={index}>
-                            <h3>{item.title}</h3>
-                            <p>{item.description}</p>
-                            <div className="Grey"></div>
-                            <img src={item.image}></img>
-                        </div>
-                    )
-                }
-                {/* {computerScienceData.map((item, index) =>
-                    <div className="ContentCard" id={index}>
-                        <h3 className="ProjectTitle"> {item.title} </h3>
-                        <div className="ImageContainer">
-                            <img src={item.image}></img>
-                        </div>
-                        <div className="ComponentsContainer">
-                            <h4 className="ProjectSubTitle"> Components </h4>
-                            <div className="Icons">
-                                {item.documentation.map((item2, index) => 
-                                    <a href={item2.link} id={index}>
-                                        {item2.icon}
-                                    </a>
-                                )}
-                            </div>
-                        </div>
-                        <div className="TextContainer">
-                        <p>
-                            {item.description}
-                        </p>
-                        </div>
-                    </div>
-                )} */}
+                <div className="CarouselWrapper">
+                    <Carousel showArrows={true} >
+                        {
+                            computerScienceData.map((item, index) =>
+                                <div key={index}>
+                                    <img src={item.image}/>
+                                    <div className="legend">
+                                        <h1>{item.title}</h1>
+                                        <p>{
+                                            item.components.map((item2, index) => {
+                                                if (item.components.length !== (index+1))
+                                                    return item2 + ', '
+                                                return item2
+                                            })
+                                        }</p>
+                                    </div>
+                                </div>
+                            )
+                        }
+                    </Carousel>
+                </div>
             </div>
+            {/*<div className="ContentContainer">*/}
+            {/*    {*/}
+            {/*        computerScienceData.map((item, index) =>*/}
+            {/*            <div className="ContentProfile" key={index}>*/}
+            {/*                <h3>{item.title}</h3>*/}
+            {/*                <p>{item.description}</p>*/}
+            {/*                <div className="Grey"></div>*/}
+            {/*                <img src={item.image}></img>*/}
+            {/*            </div>*/}
+            {/*        )*/}
+            {/*    }*/}
+            {/*    /!* {computerScienceData.map((item, index) =>*/}
+            {/*        <div className="ContentCard" id={index}>*/}
+            {/*            <h3 className="ProjectTitle"> {item.title} </h3>*/}
+            {/*            <div className="ImageContainer">*/}
+            {/*                <img src={item.image}></img>*/}
+            {/*            </div>*/}
+            {/*            <div className="ComponentsContainer">*/}
+            {/*                <h4 className="ProjectSubTitle"> Components </h4>*/}
+            {/*                <div className="Icons">*/}
+            {/*                    {item.documentation.map((item2, index) => */}
+            {/*                        <a href={item2.link} id={index}>*/}
+            {/*                            {item2.icon}*/}
+            {/*                        </a>*/}
+            {/*                    )}*/}
+            {/*                </div>*/}
+            {/*            </div>*/}
+            {/*            <div className="TextContainer">*/}
+            {/*            <p>*/}
+            {/*                {item.description}*/}
+            {/*            </p>*/}
+            {/*            </div>*/}
+            {/*        </div>*/}
+            {/*    )} *!/*/}
+            {/*</div>*/}
             {/* <h2 className="CardSubtitle">Robotics</h2>
             <div className="ContentContainer">
                 {
